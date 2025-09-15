9622cf0e3c2d3575fd5602db3478dda5f9a3da29
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2025-09-15 13:10:26 -0300

    Implementa lógica do jogo da velha
    Substitui botões vazios por botões interativos no arquivo `MainPage.xaml`, adicionando eventos `Clicked` para cada botão.
    
    No arquivo `MainPage.xaml.cs`, adiciona a variável `jogadorAtual` para rastrear o jogador ativo e implementa o método `Button_Clicked` para alternar entre os jogadores e atualizar o texto dos botões com "X" ou "O".

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,14 +2,30 @@
 {
     public partial class MainPage : ContentPage
     {
- 
+        string jogadorAtual = "X";
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-     
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            if (jogadorAtual == "X")
+            {
+                btn.Text = "X";
+                jogadorAtual = "O";
+            }
+            else
+            {
+                btn.Text = "O";
+                jogadorAtual = "X";
+            }
+
+        }
+
     }
 
 }
