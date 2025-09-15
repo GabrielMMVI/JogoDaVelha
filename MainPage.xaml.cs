7fc144853feac147c0b77121f131c27e240209f7
Gabriel Junqueira <161396206+GabrielMMVI@users.noreply.github.com>
2025-09-15 14:01:41 -0300

    Desabilita botão e adiciona verificação de vitória
    Implementa lógica para desabilitar o botão clicado e verificar se o jogador "X" venceu. Adiciona o método `Zerar()` para limpar os botões e reabilitá-los para uma nova partida. Exibe uma mensagem de alerta ao vencer.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -12,6 +12,7 @@
         private void Button_Clicked(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
+            btn.IsEnabled = false;
 
             if (jogadorAtual == "X")
             {
@@ -24,8 +25,39 @@
                 jogadorAtual = "X";
             }
 
+            if (btn10.Text == "X" && btn11.Text == "X" && btn12.Text == "X")
+            {
+                DisplayAlert("Parabéns", "Jogador X venceu!", "OK");
+                Zerar();
+            }
+            
+
+        }
+        void Zerar()
+        {
+            btn10.Text = "";
+            btn11.Text = "";
+            btn12.Text = "";
+            btn20.Text = "";
+            btn21.Text = "";
+            btn22.Text = "";
+            btn30.Text = "";
+            btn31.Text = "";
+            btn32.Text = "";
+
+            btn10.IsEnabled = true;
+            btn11.IsEnabled = true;
+            btn12.IsEnabled = true;
+            btn20.IsEnabled = true;
+            btn21.IsEnabled = true;
+            btn22.IsEnabled = true;
+            btn30.IsEnabled = true;
+            btn31.IsEnabled = true;
+            btn32.IsEnabled = true;
+
+
         }
 
-    }
 
+    }
 }
